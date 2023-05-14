f1d2965928e5a945f7ffd77ff535ee9eab0ecd26
Neen <neen.pongsakul@allianz.com>
2023-05-14 21:31:33 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-tree-children-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'nx-tree-children';
+}
